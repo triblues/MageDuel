c200ad6f086978fd2f2543f39bbf9f7ff474d68e
shawn876066 <shawn_kong_303@hotmail.com>
2015-11-07 13:08:20 +0800

    Edited game settings page
    Corrected resolution change functions so that only screen width and
    height is changed when a resolution option is clicked on

--- MageDuel/Assets/script/gameSettings.cs
+++ MageDuel/Assets/script/gameSettings.cs
@@ -6,15 +6,32 @@ using System.Collections;
 
 public class gameSettings : MonoBehaviour {
 
+    int width;
+    int height;
+
+    bool fullscreen = false;
+
     // Changes the screen resolution to 800 x 600 pixels
     public void changeTo800by600pixels ()
     {
-        Screen.SetResolution(800, 600, true);
+        Screen.SetResolution(800, 600, false);
     }
 
     // Changes the screen resolution to 1024 x 768 pixels
     public void changeTo1024by768pixels ()
     {
-        Screen.SetResolution(1024, 768, true);
+        Screen.SetResolution(1024, 768, false);
+    }
+
+    // Changes the game to full screen
+    public void changeToFullScreen (int width, int height)
+    {
+        Screen.SetResolution(width, height, true);
+    }
+
+    // Changes the game to windowed screen
+    public void changeToWindowedScreen (int width, int height, bool fullscreen)
+    {
+        Screen.SetResolution(width, height, false);
     }
 }
